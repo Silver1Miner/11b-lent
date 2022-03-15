2d49411a3ea9c8221ae0d6f8f434b8a344464db7
User <silver1miner@gmail.com>
2022-03-14 22:58:20 -0700

    act 2 scenes

--- data/act2.gd
+++ data/act2.gd
@@ -11,7 +11,7 @@ var scene_8 := { # to scene 9
 "3": {"name": "Mark", "profile": "",
 "text": "The Old Man has been disappearing off more and more ever since. Lately it's just been me and Lily in the shop."},
 "4": {"name": "Mark", "profile": "",
-"text": "There is one downside of Lily being so nice, however."},
+"text": "Speaking of which... I glance up at the clock, and right on cue, there is a knock."},
 "5": {"name": "Lily", "profile": "lily",
 "text": "'Hi Mark, mind if take my lunch break in here with you?'"},
 "6": {"name": "Mark", "profile": "lily",
@@ -20,28 +20,90 @@ var scene_8 := { # to scene 9
 "text": "Rob almost never came to the back office, and I almost never went out from it."},
 "8": {"name": "Mark", "profile": "lily",
 "text": "Lily is different. She's always coming back here during her breaks, trying to make idle chat."},
-"9": {"name": "Mark", "profile": "lily",
-"text": "I try to keep up as best I can."},
-"10": {"name": "Lily", "profile": "lily",
-"text": "."},
+"9": {"name": "Lily", "profile": "lily",
+"text": "'Hey, you brought a new book!'"},
+"10": {"name": "Mark", "profile": "lily",
+"text": "I'm genuinly surprised that she noticed."},
+"11": {"name": "Mark", "profile": "lily",
+"text": "'Oh, uh, yeah, sometimes I bring books to read because there's a lot of downtime here in the back."},
+"12": {"name": "Mark", "profile": "lily",
+"text": "'The boss actually suggested the idea in the first place. He's a pretty lax guy."},
+"13": {"name": "Lily", "profile": "lily",
+"text": "'Hmmm... Yeah, the boss is a pretty interesting figure..."},
+"14": {"name": "Lily", "profile": "lily",
+"text": "'I'm curious to hear more about your experience with him.'"},
+"15": {"name": "Lily", "profile": "lily",
+"text": "'Oh, but I'm also curious about the book you're reading too!'"},
 },
 }
 
-var choice_2 = ["I","I'm writing a book"]
-var scene_9 : = {}
-var scene_10 := {}
-var scene_11 := {}
+var choice_2 = ["Talk about the old man","Talk about books"]
+var scene_9 : = {
+"0": {"name": "Mark", "profile": "lily",
+"text": "'Well, I wouldn't say I know the old man that well. I will say he's a good guy though.'"},
+"1": {"name": "Mark", "profile": "lily",
+"text": "'He really helped me when I was in a bad place in my life by giving me this job.'"},
+"2": {"name": "Lily", "profile": "lily",
+"text": "'Heh, that actually sounds a little like how it was with me.'"},
+"3": {"name": "Lily", "profile": "lily",
+"text": "'I was in a pretty tight spot when the boss offered me this job.'"},
+"4": {"name": "Lily", "profile": "lily",
+"text": "'It felt like such perfect timing that it was actually almost suspicious.'"},
+"5": {"name": "Mark", "profile": "lily",
+"text": "'The old man certainly is a mysterious character. Like how he's even called 'old man' without looking very old.'"},
+"6": {"name": "Lily", "profile": "lily",
+"text": "'Well... I have my theories...'"},
+"7": {"name": "Mark", "profile": "lily",
+"text": "'Oh? Do share.'"},
+"8": {"name": "Lily", "profile": "lily",
+"text": "'He's an alien!'"},
+"9": {"name": "Lily", "profile": "lily",
+"text": "'Think about it. He's always disappearing off to unexplained places.'"},
+"10": {"name": "Lily", "profile": "lily",
+"text": "'His office is full of astrophysics and space books.'"},
+"11": {"name": "Lily", "profile": "lily",
+"text": "'He's old but looks young due to wearing a young person skin suit.'"},
+"12": {"name": "Lily", "profile": "lily",
+"text": "'It's the only explanation!'"},
+"13": {"name": "Mark", "profile": "lily",
+"text": "I can't help but smile."},
+"14": {"name": "Mark", "profile": "lily",
+"text": "Against my better judgment, I can't but feel almost fond of her."},
+}
+var scene_10 := {
+"0": {"name": "Mark", "profile": "",
+"text": "Well, this new book is a romance I've been meaning to read."},
+"1": {"name": "Mark", "profile": "",
+"text": "."},
+
+
+"13": {"name": "Mark", "profile": "lily",
+"text": "I can't help but smile."},
+"14": {"name": "Mark", "profile": "lily",
+"text": "Against my better judgment, I can't but feel almost fond of her."},
+}
+
+var scene_11 := {
+"0": {"name": "Lily", "profile": "",
+"text": "I'm a sucker for a good romance story."},
+"1": {"name": "Mark", "profile": "",
+"text": "Is she really asking to see my house?"},
+}
 var scene_12 := {}
-var choice_3 = ["Talk about sister", "Admit to being colorblind"]
+var choice_3 = ["Talk about sister", "Talk about being colorblind"]
 var scene_13 := {
 "0": {"name": "Mark", "profile": "",
 "text": "That room was my sister's room."},
 "1": {"name": "Mark", "profile": "",
-"text": "She passed away many years ago."},
+"text": "She... passed away."},
 "2": {"name": "Lily", "profile": "",
 "text": "'Oh. I'm sorry.'"},
 "3": {"name": "Mark", "profile": "",
 "text": "It was a long time ago."},
+"4": {"name": "Mark", "profile": "",
+"text": "..."},
+"5": {"name": "Lily", "profile": "",
+"text": "Would you like me to change the subject?"},
 }
 var scene_14 := {
 "0": {"name": "Mark", "profile": "",
@@ -55,14 +117,19 @@ var scene_14 := {
 "4": {"name": "Mark", "profile": "",
 "text": "'I started writing notes to remind myself what colors different things were.'"},
 }
-var scene_15 := {}
+var scene_15 := {
+"0": {"name": "Lily", "profile": "",
+"text": "Thanks for opening up to me. I feel like I should open up a little too, so it'd be fair."},
+"1": {"name": "Mark", "profile": "",
+"text": "Oh, you shouldn't feel obligated."},
+}
 
 var choice_4 = ["Encourage her to make it work","Persuade her against it."]
 var scene_16 := {
 "0": {"name": "Mark", "profile": "",
 "text": "It's obvious what you should do."},
 "1": {"name": "Mark", "profile": "",
-"text": "Long distance is a challenge."},
+"text": "Long distance almost never works."},
 "2": {"name": "Mark", "profile": "",
 "text": "'But aren't all the best romance stories about overcoming those challenges with your partner?'"},
 }
@@ -70,5 +137,5 @@ var scene_17 := {
 "0": {"name": "Mark", "profile": "",
 "text": "It's obvious what you should do."},
 "1": {"name": "Mark", "profile": "",
-"text": "Long distance never works."},
+"text": "Long distance almost never works."},
 }
